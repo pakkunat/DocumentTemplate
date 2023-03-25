f1f6b1280a469a40e9d5bf38757312be4fdc951b
pakkunat <pakkun_kh@fol.hi-ho.ne.jp>
2023-03-25 23:50:19 +0900

    updated

--- main.js
+++ main.js
@@ -10,6 +10,5 @@ function main() {
   body.insertParagraph(0, "タイトル").setHeading(DocumentApp.ParagraphHeading.TITLE);
   body.insertParagraph(1, "サブタイトル").setHeading(DocumentApp.ParagraphHeading.SUBTITLE);
   body.insertParagraph(2, Utilities.formatString("最終更新日 %s", PApp.getJapanDate(new Date()))).setAlignment(DocumentApp.HorizontalAlignment.RIGHT);
-  body.insertParagraph(3, "出典");
-  body.insertParagraph(4, "リンク");
+  body.insertParagraph(3, "リンク");
 }
